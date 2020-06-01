29e6e7185ffa1d0b2f4e5c7b4039912e60e17d71
Benhur Külzer <benhur.bitpc@gmail.com>
2020-06-01 01:21:01 -0300

    Adjusts on: Schedule List, API conection, Showing schedules, calendar, next, header, profile page, avatar change and update profile data

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,34 +1,45 @@
 import React, { createContext, useCallback, useState, useContext } from 'react';
+
 import api from '../services/api';
+
+interface User {
+  id: string;
+  avatar_url: string;
+  name: string;
+  email: string;
+}
 
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
-interface SignInCredentials {
+interface SignInCredencials {
   email: string;
   password: string;
 }
 
 interface AuthContextData {
-  user: object;
-  signIn(credentials: SignInCredentials): Promise<void>;
+  user: User;
+  signIn(credentials: SignInCredencials): Promise<void>;
   signOut(): void;
+  updateUser(user: User): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
-const AuthProvider: React.FC = ({ children }) => {
+export const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>(() => {
     const token = localStorage.getItem('@GoBarber:token');
     const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
-      return { token, user: JSON.parse(user) }
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
+      return { token, user: JSON.parse(user) };
     }
 
-    return {} as AuthState; //este trecho acima somente é executado quando o usuário da refresh ou entra e sai da aplicação
+    return {} as AuthState;
   });
 
   const signIn = useCallback(async ({ email, password }) => {
@@ -42,31 +53,45 @@ const AuthProvider: React.FC = ({ children }) => {
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
 
+    api.defaults.headers.authorization = `Bearer ${token}`;
+
     setData({ token, user });
   }, []);
 
   const signOut = useCallback(() => {
-    localStorage.removeItem('@Gobarber:token');
-    localStorage.removeItem('@Gobarber:user');
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
 
     setData({} as AuthState);
   }, []);
 
+  const updateUser = useCallback(
+    (user: User) => {
+      localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+      setData({
+        token: data.token,
+        user,
+      });
+    },
+    [data.token],
+  );
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ user: data.user, signIn, signOut, updateUser }}
+    >
       {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
-function useAuth(): AuthContextData {
+export function useAuth(): AuthContextData {
   const context = useContext(AuthContext);
 
   if (!context) {
-    throw new Error('useAuth must be used within an authProvider');
+    throw new Error('useAuth must be used within an AuthProvider');
   }
 
   return context;
 }
-
-export { AuthProvider, useAuth };
--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -3,7 +3,7 @@ import { FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup  from 'yup';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import { useToast } from '../../hooks/toast';
 import getValidationErrors from '../../utils/getValidationErrors';
@@ -14,6 +14,7 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Content, Backgroung, AnimationContainer } from './styles';
+import api from '../../services/api';
 
 interface ResetPasswordFormData {
   password: string;
@@ -25,6 +26,7 @@ const ResetPassword: React.FC = () => {
 
   const { addToast } = useToast();
   const history = useHistory();
+  const location = useLocation();
 
   const handleSubmit = useCallback(async  (data: ResetPasswordFormData) => {
     try {
@@ -39,7 +41,20 @@ const ResetPassword: React.FC = () => {
         abortEarly: false,
       });
 
-      history.push('/signin');
+      const { password, password_confirmation } = data;
+      const token = location.search.replace('?token=', '');
+
+      if (!token) {
+        throw new Error();
+      }
+
+      await api.post('/password/reset', {
+        password,
+        password_confirmation,
+        token,
+      });
+
+      history.push('/');
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
@@ -56,13 +71,13 @@ const ResetPassword: React.FC = () => {
         description: 'Ocorreu um erro ao resetar sua senha, tente novamente',
       });
     }
-  }, [addToast, history]);
+  }, [addToast, history, location]);
 
   return (
     <Container>
       <Content>
         <AnimationContainer>
-          <img src={logoImg} alt="GoBarber" />
+          <img src={logoImg} alt="RocksBarber" />
 
           <Form ref={formRef} onSubmit={handleSubmit}>
             <h1>Resetar senha</h1>
